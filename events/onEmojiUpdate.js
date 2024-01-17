8e2b167f36913726eb7d614b8b1a22280bcb6c4d
marshallovski <68496774+marshallovski@users.noreply.github.com>
2024-01-17 21:17:33 +0200

    removed buffer check

--- events/onEmojiUpdate.js
+++ events/onEmojiUpdate.js
@@ -1,5 +1,4 @@
 exports.onEmojiUpdate = async (ws, data, emojiList, parsedData) => {
-    if (ws.bufferedAmount === 0)
         ws.send(JSON.stringify({
             author: Buffer.from(parsedData(data).author, 'base64').toString(),
             emoji: emojiList,
