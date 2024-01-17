cd3106d4b38ab3dcb72354c1180f88218026a30a
marshallovski <68496774+marshallovski@users.noreply.github.com>
2024-01-17 21:18:01 +0200

    removed buffer check

--- events/onHTMLEmoji.js
+++ events/onHTMLEmoji.js
@@ -1,6 +1,5 @@
 exports.onHTMLEmoji = async (wss, ws, escapeHTML, parsedData, data) => {
     wss.clients.forEach(client => {
-        if (ws.bufferedAmount === 0)
             client.send(JSON.stringify({
                 author: escapeHTML(Buffer.from(parsedData(data).author, 'base64').toString()),
                 type: 'msg',
