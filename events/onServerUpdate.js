9bd8d0bd576bcc0607334f05e62d9743c255fd63
marshallovski <68496774+marshallovski@users.noreply.github.com>
2024-01-17 21:18:52 +0200

    removed buffer check

--- events/onServerUpdate.js
+++ events/onServerUpdate.js
@@ -1,5 +1,4 @@
 exports.onServerUpdate = async (ws, editJsonFile, config, parsedData, data) => {
-    if (ws.bufferedAmount === 0)
         return ws.send(JSON.stringify({
             author: Buffer.from(parsedData(data).author, 'base64').toString(),
             type: 'srvupdate',
